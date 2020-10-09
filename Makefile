393c69adfaffeb4fde111663e4adc9657426e81d
Lahloug <ghali-lahlou998@hotmail.com>
2020-10-09 17:45:15 +0200

    Makefile hello.asm [REPLACE] _main with _start

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: clean
 	@nasm -fmacho64 hello.asm
-	@ld -o hello hello.o -lSystem
+	@ld -e _start -o hello hello.o -lSystem
 	@./hello
 clean:
 	@rm -f hello.o hello
--- hello.asm
+++ hello.asm
@@ -1,7 +1,7 @@
-          global    _main
+          global    _start
 
           section   .text
-_main:    mov       rax, 0x02000004         ; system call for write
+_start:   mov       rax, 0x02000004         ; system call for write
           mov       rdi, 1                  ; file handle 1 is stdout
           mov       rsi, message            ; address of string to output
           mov       rdx, 13                 ; number of bytes
@@ -11,4 +11,4 @@ _main:    mov       rax, 0x02000004         ; system call for write
           syscall                           ; invoke operating system to exit
 
           section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+message:  db        'Hello, World', 10      ; note the newline at the end
