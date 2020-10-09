ac79551952274a9d14c79c8e33c247d09925726d
Lahloug <ghali-lahlou998@hotmail.com>
2020-10-09 21:09:21 +0200

    hello.asm [UPDATE] ask for user name

--- hello.asm
+++ hello.asm
@@ -1,14 +1,33 @@
           global    _start
 
+          section   .data
+text1:    db        'What is your name ?', 10      ; note the newline at the end
+text2:    db        'Hello, '
+
+          section   .bss
+name:     resb        16
+
           section   .text
 _start:   mov       rax, 0x02000004         ; system call for write
           mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
+          mov       rsi, text1              ; address of string to output
+          mov       rdx, 20                 ; number of bytes
+          syscall
+          mov       rax, 0x02000003         ; system call for read
+          mov       rdi, 0                  ; file handle 0 is stdin
+          mov       rsi, name               ; address of string to store
+          mov       rdx, 16                 ; number of bytes
+          syscall
+          mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, text2              ; address of string to output
+          mov       rdx, 7                  ; number of bytes
+          syscall
+          mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, name               ; address of string to output
+          mov       rdx, 16                 ; number of bytes
+          syscall
           mov       rax, 0x02000001         ; system call for exit
           xor       rdi, rdi                ; exit code 0
           syscall                           ; invoke operating system to exit
-
-          section   .data
-message:  db        'Hello, World', 10      ; note the newline at the end
